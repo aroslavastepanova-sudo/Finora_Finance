dabfeac1690957548e872c23b6ee648f82cd3f4e
Admin <aroslavastepanova@gmail.com>
2026-09-23 13:41:58 +0800

    База данных

--- app.py
+++ app.py
@@ -1,13 +1,3 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
